0bc595ad44c565f9c0188347e70853b29e5173f7
Ashish Pandey <tech4abetterfuture@gmail.com>
2018-11-19 17:59:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "local_file" "foo" {
     content     = "${var.AUTHOR} ${var.mylocal1}"
     filename = "${path.module}/foo.bar"
 }
+variable "AUTHOR" {
+  description = "Name of container"
+  default = "def"
+}
+variable "mylocal1" {
+  description = "Name of container"
+  default = "def"
+}
